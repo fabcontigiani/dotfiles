4fd33a0e36934194456a732a2901ae26b1aaa5e4
Fabrizio Conttigiani <fabcontigiani@gmail.com>
2022-07-31 14:42:41 -0300

    Misc

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -23,7 +23,8 @@
 ;;
 (setq doom-font (font-spec :family "Iosevka Term" :size 16)
       doom-variable-pitch-font (font-spec :family "Iosevka Aile" :size 16)
-      doom-serif-font (font-spec :family "Iosevka Etoile" :size 16))
+      doom-serif-font (font-spec :family "Iosevka Etoile" :size 16)
+      doom-unicode-font (font-spec :family "Iosevka Nerd Font" :size 16))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
