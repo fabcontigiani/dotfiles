18ca345bdc41649f045050673386a53835c645b4
Fabrizio Contigiani <fabcontigiani@gmail.com>
2024-03-08 14:48:01 -0300

    Use suggested config from repo

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -880,45 +880,56 @@
 (use-package org-roam
   :after org
   :config
-  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (require 'org-roam-dailies)
   (org-roam-db-autosync-mode)
+  (setq org-roam-node-display-template
+        (concat "${title:80} " (propertize "${tags:20}" 'face 'org-tag))
+        org-roam-node-annotation-function
+        (lambda (node) (marginalia--time (org-roam-node-file-mtime node))))
   :custom
   (org-roam-directory (concat org-directory "roam"))
-  (org-roam-capture-templates
-   '(("d" "default" plain
-      "%?" :target
-      (file+head "${slug}.org" "#+title: ${title}\n")
-      :unnarrowed t)))
   (org-roam-completion-everywhere t)
-  :bind (("C-c n t" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n r" . org-roam-node-random)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ("C-c n j" . org-roam-dailies-capture-today)
-         ("C-c n e" . consult-org-roam-file-find)
-         ("C-c n b" . consult-org-roam-backlinks)
-         ("C-c n l" . consult-org-roam-forward-links)
-         ("C-c n s" . consult-org-roam-search)))
+  (org-roam-capture-templates
+   '(("d" "default" entry  "* %?"
+      :target (file+head "${id}--${slug}.org"
+                         "#+title: ${title}\n#+date: %T\n#+filetags: %^g\n")
+      :unnarrowed t)))
+  :bind
+  ("C-c n t" . org-roam-buffer-toggle)
+  ("C-c n f" . org-roam-node-find)
+  ("C-c n r" . org-roam-node-random)
+  ("C-c n g" . org-roam-graph)
+  ("C-c n i" . org-roam-node-insert)
+  ("C-c n c" . org-roam-capture)
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map))
 
 (use-package consult-org-roam
-  :after org-roam
-  :init
-  (require 'consult-org-roam)
-  ;; Activate the minor mode
-  (consult-org-roam-mode 1)
-  :custom
-  ;; Use `ripgrep' for searching with `consult-org-roam-search'
-  (consult-org-roam-grep-func #'consult-ripgrep)
-  ;; Configure a custom narrow key for `consult-buffer'
-  (consult-org-roam-buffer-narrow-key ?r)
-  ;; Display org-roam buffers right after non-org-roam buffers
-  ;; in consult-buffer (and not down at the bottom)
-  (consult-org-roam-buffer-after-buffers t)
-  :config
-  ;; Eventually suppress previewing for certain functions
-  (consult-customize consult-org-roam-forward-links :preview-key "M-."))
+   :after org-roam
+   :init
+   (require 'consult-org-roam)
+   ;; Activate the minor mode
+   (consult-org-roam-mode 1)
+   :custom
+   ;; Use `ripgrep' for searching with `consult-org-roam-search'
+   (consult-org-roam-grep-func #'consult-ripgrep)
+   ;; Configure a custom narrow key for `consult-buffer'
+   (consult-org-roam-buffer-narrow-key ?r)
+   ;; Display org-roam buffers right after non-org-roam buffers
+   ;; in consult-buffer (and not down at the bottom)
+   (consult-org-roam-buffer-after-buffers t)
+   :config
+   ;; Eventually suppress previewing for certain functions
+   (consult-customize
+    consult-org-roam-forward-links
+    :preview-key "M-.")
+   :bind
+   ;; Define some convenient keybindings as an addition
+   ("C-c n F" . consult-org-roam-file-find)
+   ("C-c n b" . consult-org-roam-backlinks)
+   ("C-c n B" . consult-org-roam-backlinks-recursive)
+   ("C-c n l" . consult-org-roam-forward-links)
+   ("C-c n s" . consult-org-roam-search))
 
 (use-package bibtex
   :defer t
