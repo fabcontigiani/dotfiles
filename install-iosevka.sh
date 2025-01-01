b71d2e610a9f12d692e6f7f7805cf88d9dab62dd
Fabrizio Contigiani <fabcontigiani@gmail.com>
2025-01-01 20:22:18 +0000

    Update Iosevka install script

--- install-iosevka.sh
+++ install-iosevka.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir /tmp/iosevka-font
+mkdir -p /tmp/iosevka-font
 cd /tmp/iosevka-font
 curl -s 'https://api.github.com/repos/be5invis/Iosevka/releases/latest' | jq -r ".assets[] | .browser_download_url" | grep PkgTTC-Iosevka | xargs -n 1 curl -L -O --fail --silent --show-error
 unzip \*.zip
-sudo mkdir /usr/local/share/fonts/iosevka-font
+sudo mkdir -p /usr/local/share/fonts/iosevka-font
 sudo mv *.ttc /usr/local/share/fonts/iosevka-font/
-fc-cache
+sudo fc-cache -fv
